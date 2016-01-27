ac65181a3b30b5c7f091d9926b036021fc28edb5
Philip Newborough <corenominal@corenominal.org>
2016-01-27 21:45:27 +0000

    Theme comments and readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# corenominal-wp-api
-A WordPress plugin to provide extra functionality for my site
+# corenominal's WordPress API Plugin
+
+A WordPress plugin to provide extra functionality for my [website](https://corenominal.org). It provides features designed specifically for my needs, but feel free to take a look.
---
+++ corenominal-wp-api.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: corenominal's WordPress API
+ * Description: This is corenominal's WordPress API. It provides features specifically for corenominal, but feel free to take a look.
+ * Plugin URI: https://github.com/corenominal/corenominal-wp-api
+ * Author: Philip Newborough
+ * Author URI: http://corenominal.org
+ * Version: 0.0.1
+ * License: GPLv2
+ */
