de665b70bcc02efd10a503ad513fec44ceb5a3b0
Philip Newborough <corenominal@corenominal.org>
2016-02-13 15:41:43 +0000

    Remove doodle download endpoint

--- endpoints.php
+++ endpoints.php
@@ -9,12 +9,6 @@ function corenominal_api_register_endpoints()
         'methods' => 'GET',
         'callback' => 'corenominal_api_ip',
     ));
-
-    // endpoint:/wp-json/corenominal/doodle_download
-	register_rest_route( 'corenominal', '/doodle_download', array(
-        'methods' => 'GET',
-        'callback' => 'corenominal_api_doodle_download',
-    ));
 }
 add_action( 'rest_api_init', 'corenominal_api_register_endpoints' );
 
@@ -22,8 +16,3 @@ add_action( 'rest_api_init', 'corenominal_api_register_endpoints' );
  * Endpoint: /wp-json/corenominal/ip
  */
 require_once( plugin_dir_path( __FILE__ ) . 'endpoints/ip.php' );
-
-/**
- * Endpoint: /wp-json/corenominal/doodle_download
- */
-require_once( plugin_dir_path( __FILE__ ) . 'endpoints/doodle_download.php' );
