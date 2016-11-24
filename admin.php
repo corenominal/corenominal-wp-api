0eb878090dccf30f1b9ca6941b5a7c6c1171b556
Philip Newborough <corenominal@corenominal.org>
2016-11-24 08:40:35 +0000

    Refactor slugs

--- admin.php
+++ admin.php
@@ -11,10 +11,10 @@ function corenominal_api_admin_options()
 {
 	// Add top level page
 	add_menu_page(
-		'corenominal API Options', // page title
+		'corenominal API Info', // page title
 		'corenominal', // menu title
 		'manage_options', // capability
-		'corenominal_options', // slug
+		'corenominal_api_info', // slug
 		'corenominal_api_info_callback', // callback
 		'dashicons-admin-settings', // icon - 20x20 png or SVG - for dashicon just ref the icon 'dashicons-sos'
 		60 //position
@@ -23,11 +23,11 @@ function corenominal_api_admin_options()
 	// Add sub menu page
 	// Note, the first submenu item needs to copy the top level.
 	add_submenu_page(
-		'corenominal_options', // parent slug
-		'corenominal API Options', // page title
+		'corenominal_api_info', // parent slug
+		'corenominal API Info', // page title
 		'Information', // menu title
 		'manage_options', // capability
-		'corenominal_options', // slug
+		'corenominal_api_info', // slug
 		'corenominal_api_info_callback' // callback function
 	);
 
@@ -36,7 +36,7 @@ function corenominal_api_admin_options()
 
 	// Add sub menu page for generating the API key
 	add_submenu_page(
-		'corenominal_options', // parent slug
+		'corenominal_api_info', // parent slug
 		'corenominal - API Key', // page title
 		'API Key', // menu title
 		'manage_options', // capability
@@ -49,7 +49,7 @@ function corenominal_api_admin_options()
 
 	// Add sub menu page for listing my GitHub hosted WP plugins
 	add_submenu_page(
-		'corenominal_options', // parent slug
+		'corenominal_api_info', // parent slug
 		'corenominal - GitHub Hosted WP Plugins', // page title
 		'WP Plugins', // menu title
 		'manage_options', // capability
